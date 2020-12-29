ba08cdf8900b4e6d0cf5e7bb9ba9bef9c0cce385
Julian Theis <julian.theis@posteo.de>
2020-12-29 15:15:36 +0100

    Simplify config handling

--- src/gymscraper/config.py
+++ src/gymscraper/config.py
@@ -12,23 +12,23 @@ defaultConfig = {
         {
             "name": "greifhaus",
             "url": "https://www.boulderado.de/boulderadoweb/gym-clientcounter/index.php?mode=get&token=eyJhbGciOiJIUzI1NiIsICJ0eXAiOiJKV1QifQ.eyJjdXN0b21lciI6IkdyZWlmaGF1cyJ9.3Nen_IU5N2sVtJbP44CGCFfdKY93zQx2FRczY4z9Jy0",
-            "type": Gym.BOULDERADO,
+            "type": Gym.BOULDERADO.value,
         },
         {
             "name": "fliegerhalle",
             "url": "https://158.webclimber.de/de/trafficlight?callback=WebclimberTrafficlight.insertTrafficlight&key=yspPh6Mr2KdST3br8WC7X8p6BdETgmPn&hid=158&container=trafficlightContainer&type=&area=",
-            "type": Gym.WEBCLIMBER,
+            "type": Gym.WEBCLIMBER.value,
         },
         {
             "name": "the-spot-boulder",
             "url": "https://portal.rockgympro.com/portal/public/415a34a23151c6546419c1415d122b61/occupancy?&iframeid=occupancyCounter&fId=",
-            "type": Gym.ROCKGYMPRO,
+            "type": Gym.ROCKGYMPRO.value,
             "location": "BLD",
         },
         {
             "name": "the-spot-denver",
             "url": "https://portal.rockgympro.com/portal/public/415a34a23151c6546419c1415d122b61/occupancy?&iframeid=occupancyCounter&fId=",
-            "type": Gym.ROCKGYMPRO,
+            "type": Gym.ROCKGYMPRO.value,
             "location": "DEN",
         },
     ],
@@ -36,53 +36,75 @@ defaultConfig = {
 }
 
 
-def loadConfig(filename: str) -> Optional[dict]:
-    # check if file exists
-    if not os.path.isfile(filename):
-        logging.error('File "%s" not found. Creating empty config file, please fill in the empty fields', filename)
-        createEmptyConfig(filename, defaultConfig)
-        return None
+# def loadConfig(filename: str) -> Optional[dict]:
+#     # check if file exists
+#     if not os.path.isfile(filename):
+#         logging.error('File "%s" not found. Creating empty config file, please fill in the empty fields', filename)
+#         createEmptyConfig(filename, defaultConfig)
+#         return None
+# 
+#     # try loading the config
+#     with open(filename, "r") as configFile:
+#         try:
+#             config = json.load(configFile)
+#         except json.decoder.JSONDecodeError:
+#             logging.error("Corrupt config. Creating empty config file, please fill in the empty fiels")
+#             createEmptyConfig(filename, defaultConfig)
+#             return None
+# 
+#     # check all fields exists
+#     for key in defaultConfig:
+#         if not key in config or type(defaultConfig[key]) is not type(config[key]):
+#             logging.error('File "mail.config" incomplete. %s is missing or invalid. Renaming old config file and generating new config', key)
+#             createEmptyConfig(filename, defaultConfig)
+#             return None
+# 
+#     # return valid config
+#     return config
+# 
+# 
+# def createEmptyConfig(filename: str, defaultConfig: dict):
+#     saveOldConfig(filename)
+# 
+#     with open(filename, "w") as configFile:
+#         json.dump(defaultConfig, configFile)
+# 
+# 
+# def saveOldConfig(filename: str):
+#     if not os.path.exists(filename):
+#         return
+# 
+#     targetFileName = "{0}_{1}.json".format(
+#         os.path.splitext(filename)[0], getTimeForFilename()
+#     )
+# 
+#     if os.path.exists(targetFileName):
+#         os.remove(targetFileName)
+# 
+#     os.rename(filename, targetFileName)
+# 
+# 
+# def getTimeForFilename():
+#     return datetime.now().strftime("%Y-%m-%d-%H%M%S")
 
-    # try loading the config
-    with open(filename, "r") as configFile:
-        try:
-            config = json.load(configFile)
-        except json.decoder.JSONDecodeError:
-            logging.error("Corrupt config. Creating empty config file, please fill in the empty fiels")
-            createEmptyConfig(filename, defaultConfig)
-            return None
 
-    # check all fields exists
-    for key in defaultConfig:
-        if not key in config or type(defaultConfig[key]) is not type(config[key]):
-            logging.error('File "mail.config" incomplete. %s is missing or invalid. Renaming old config file and generating new config', key)
-            createEmptyConfig(filename, defaultConfig)
-            return None
+def load_config(filename: str) -> Optional[dict]:
+    try:
+        with open(filename, "r") as f:
+            config = json.load(f)
+            return config
+    except FileNotFoundError as e:
+        logging.error("File %s not found: %s", filename, e)
+    except json.decoder.JSONDecodeError as e:
+        logging.error("Could not load JSON: %s", e)
 
-    # return valid config
-    return config
+    return None
 
 
-def createEmptyConfig(filename: str, defaultConfig: dict):
-    saveOldConfig(filename)
-
-    with open(filename, "w") as configFile:
-        json.dump(defaultConfig, configFile)
+def generate_config(filename: str, config: dict):
+    with open(filename, "w") as f:
+        json.dump(config, f)
 
 
-def saveOldConfig(filename: str):
-    if not os.path.exists(filename):
-        return
-
-    targetFileName = "{0}_{1}.json".format(
-        os.path.splitext(filename)[0], getTimeForFilename()
-    )
-
-    if os.path.exists(targetFileName):
-        os.remove(targetFileName)
-
-    os.rename(filename, targetFileName)
-
-
-def getTimeForFilename():
-    return datetime.now().strftime("%Y-%m-%d-%H%M%S")
+def verify_config(config: dict):
+    return all(key in config and isinstance(defaultConfig[key], type(value)) for key, value in defaultConfig.items())
--- src/gymscraper/main.py
+++ src/gymscraper/main.py
@@ -1,7 +1,7 @@
 import logging
 from argparse import ArgumentParser
 
-from .config import loadConfig
+from .config import load_config, generate_config, verify_config, defaultConfig
 from .scraper import parseTarget
 
 
@@ -11,10 +11,17 @@ def main():
 
     parser = ArgumentParser(description="Scrape gym visitor count")
     parser.add_argument("-c", "--config", type=str, default="config.json", help="configuration file", metavar="FILE")
+    parser.add_argument("--generate-config", type=str, help="generate default configuration file", metavar="FILE")
     args = parser.parse_args()
 
-    config = loadConfig(args.config)
-    if not config:
+    if args.generate_config is not None:
+        logging.debug("Generating configuration file %s", args.generate_config)
+        generate_config(args.generate_config, defaultConfig)
+        return
+
+    config = load_config(args.config)
+    if config is None or not verify_config(config):
+        logging.error("Configuration file %s is invalid", args.config)
         return
 
     outputDir = config["outputDir"]
